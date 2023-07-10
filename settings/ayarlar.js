5062dc5f47aee0d1d012f1df025dfb19a601c758
solakcemaletti <139044613+solakcemaletti@users.noreply.github.com>
2023-07-10 14:38:05 +0300

    sss
    sss

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTEyNzYzNTA2NDQ0MDc1ODI3Mg.GweiLo.iFMFZROmK2WfnHMukDa_lAuwf0c57PhjCwdpxU",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "712982347267702854", //sahip id :()
+}
